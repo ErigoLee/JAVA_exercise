222a4b2f2b8cebf01f07a8abe02bcb700caaa0c6
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-26 00:30:06 +0900

    Create Exercise5.java

---
+++ Exercise5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exerise5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		final int MAX_VALUE = 100;
+		int num;
+		int sum=0;
+		
+		while (true)
+		{
+			num = sc.nextInt();
+			if (num>MAX_VALUE)
+				break;
+			sum +=num;
+		}
+		
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
